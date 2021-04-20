b56009d7a0910c2d806d54bb2a46f2fe23c9e05a
Murilo Medeiros do Valle <muthmedeiros@gmail.com>
2021-04-20 09:33:48 -0300

    level button commit

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:dev_quiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:dev_quiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,6 +13,26 @@ class _HomeState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            LevelButtonWidget(
+              label: "Fácil",
+            ),
+            LevelButtonWidget(
+              label: "Médio",
+            ),
+            LevelButtonWidget(
+              label: "Difícil",
+            ),
+            LevelButtonWidget(
+              label: "Perito",
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
